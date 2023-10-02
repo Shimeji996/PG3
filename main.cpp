c55ed949a4dd5229dac3e093a3d9abace9c6a478
Shimeji996 <k022g0021@g.neec.ac.jp>
2023-10-02 12:44:42 +0900

    master

---
+++ main.cpp
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+
+template <typename Type>
+
+Type Min(Type a, Type b) {
+    if (a < b) {
+        return a;
+    }
+    else if (b < a) {
+        return b;
+    }
+}
+
+template<>
+char Min<char>(char a, char b) {
+    printf("数字以外は代入できません\n");
+
+    return 0;
+}
+
+int main(void) {
+
+    printf("%d\n", Min<int>(114, 5));
+    printf("%f\n", Min<float>(113.5f, 5.0f));
+    printf("%lf\n",Min<double>(111, 5));
+    Min<char>('a', 'b');
+
+    return 0;
+}
