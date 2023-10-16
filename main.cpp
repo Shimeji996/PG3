2a9cb6802cef727cd5f29fd89b7dff4e99f509b5
Shimeji996 <k022g0021@g.neec.ac.jp>
2023-10-16 15:55:38 +0900

    01_03終わり

--- main.cpp
+++ main.cpp
@@ -1,34 +1,63 @@
 ﻿#include <stdio.h>
+#include <random>
+#include <chrono>
+#include <thread>
+#include <iostream>
 
-int TypicallyCalc(int time) {
-	return 1072 * time;
+std::random_device seed_Gen;
+std::mt19937 mtrand(seed_Gen());
+
+typedef int (*Pfunc)();
+
+int Dice() {
+	return std::uniform_int_distribution<int>(1, 6)(seed_Gen);
 }
 
-int RecursiveCalc(int time, int HourlyWage = 100) {
-	if (time == 1) {
-		return HourlyWage;
+void DiceResult(int diceNumber) {
+	//偶数のとき
+	if (diceNumber % 2 == 0) {
+		printf("結果　丁\n");
 	}
-	return (RecursiveCalc(time - 1, HourlyWage * 2 - 50));
+	// 奇数のとき
+	if (diceNumber % 2 == 1) {
+		printf("結果　半\n");
+	}
 }
 
-void ComparisonWage(int time) {
-	int Typically = TypicallyCalc(time);
-	int Recursive = RecursiveCalc(time);
-	printf("一般的な賃金 : %d\n", Typically);
-	printf("再帰的な賃金 : %d\n", Recursive);
-	if (Typically > Recursive) {
-		printf("一般的な賃金体系");
-	}
-	else if (Typically < Recursive) {
-		printf("再帰的な賃金体系");
-	}
-	else if (Typically == Recursive) {
-		printf("同じ");
-	}
+int SetTimeOut(Pfunc collback, int second) {
+	std::this_thread::sleep_for(std::chrono::seconds(second));
+	return collback();
+
 }
 
 int main() {
-	ComparisonWage(9);
+
+	Pfunc p;
+	p = Dice;
+	int Result;
+	int Answer;
+
+	while (true) {
+
+		printf("1:半(奇数)　2:丁(偶数)　0:終了\n");
+		std::cin >> Answer;
+		if (Answer == 0) {
+			break;
+		}
+		Result = SetTimeOut(p, 3);
+		printf("答え : %d\n", Result);
+		DiceResult(Result);
+		if (Result % 2 == 0 && Answer == 2) {
+			printf("勝ち\n");
+		}
+		else if (Result % 2 == 1 && Answer == 1) {
+			printf("勝ち\n");
+		}
+		else {
+			printf("負け\n");
+		}
+
+	}
 
 	return 0;
 }
