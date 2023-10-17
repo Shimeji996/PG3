225209c6083a74659882f6dfb6fff9007cbf9d02
Shimeji996 <k022g0021@g.neec.ac.jp>
2023-10-17 10:39:04 +0900

    01_02終わり

--- main.cpp
+++ main.cpp
@@ -11,24 +11,29 @@ int RecursiveCalc(int time, int HourlyWage = 100) {
 	return (RecursiveCalc(time - 1, HourlyWage * 2 - 50));
 }
 
-void ComparisonWage(int time) {
+void Comparison(int time) {
+
 	int Typically = TypicallyCalc(time);
 	int Recursive = RecursiveCalc(time);
+
 	printf("一般的な賃金 : %d\n", Typically);
 	printf("再帰的な賃金 : %d\n", Recursive);
+
 	if (Typically > Recursive) {
 		printf("一般的な賃金体系");
 	}
+
 	else if (Typically < Recursive) {
 		printf("再帰的な賃金体系");
 	}
+
 	else if (Typically == Recursive) {
 		printf("同じ");
 	}
 }
 
 int main() {
-	ComparisonWage(9);
+	Comparison(9);
 
 	return 0;
 }
